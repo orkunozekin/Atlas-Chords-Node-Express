4b2c6de8b85b8125ed695a73ab0fc5204810c0bb
Orkun Ozekin <orkunozekin@thinkful.com>
2020-07-03 17:04:16 -0500

    fixed heroku

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8080,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: process.env.DB_URL || 'postgresql://postgres@localhost/atlas_chords',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/atlas_chords',
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/atlas_chords_test',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret'
 }
