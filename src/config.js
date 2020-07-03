14a13b6162fb8001177e3b3907e3456d11992501
Orkun Ozekin <orkunozekin@thinkful.com>
2020-07-03 16:14:13 -0500

    ready for deploy

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8080,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DB_URL || 'postgresql://postgres@localhost/atlas_chords',
+    DATABASE_URL: process.env.DB_URL || 'postgresql://postgres@localhost/atlas_chords',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/atlas_chords_test',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret'
 }
